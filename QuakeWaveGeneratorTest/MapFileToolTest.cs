6a45225ded8e85262737e6b625c188d5a08ef211
Tim Gorgs <tim@gorgs.org>
2022-04-11 20:34:01 +0200

    - Restructure

---
+++ QuakeWaveGeneratorTest/MapFileToolTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuakeWaveGenerator;
+using System.Linq;
+
+namespace QuakeWaveGeneratorTest
+{
+    [TestClass]
+    public class MapFileToolTest
+    {
+        private readonly MapFileTool m_MapFileTool = new MapFileTool();
+
+        [TestMethod]
+        public void StepSequence_CalledWithValidArguments_ReturnsExpectedResult()
+        {
+            // Arrange
+            // Act
+            int[] sequence = m_MapFileTool.StepSequence(3, 3);
+
+            // Assert
+            Assert.AreEqual(6, sequence.Length);
+            Assert.AreEqual(0, sequence[0]);
+            Assert.AreEqual(9, sequence.Max());
+        }
+
+        [TestMethod]
+        public void StepSequence_CalledWithZeroNumberOfSteps_ReturnsEmptyArray()
+        {
+            int[] sequence = m_MapFileTool.StepSequence(0, 3);
+
+            // Assert
+            Assert.AreEqual(0, sequence.Length);
+        }
+    }
+}
---
+++ QuakeWaveGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuakeWaveGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuakeWaveGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4108f423-75bd-44a5-a1d2-8703aee7cef6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
